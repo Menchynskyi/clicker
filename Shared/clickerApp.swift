52134f72dfcf736294ccae1f70ae5f37d32e5356
Oleksanrd Menchynskyi <o.menchynskyi@geniusee.com>
2022-06-15 20:47:32 +0300

    Initial Commit

---
+++ Shared/clickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  clickerApp.swift
+//  Shared
+//
+//  Created by Oleksandr Menchynskyi on 15.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct clickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
