02f784ed28aff96bc2098c9bcc5ef974e87f9cfc
Oleksanrd Menchynskyi <o.menchynskyi@geniusee.com>
2022-06-17 13:12:46 +0300

    Refactor clicker function

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -4,63 +4,65 @@
 
 import SwiftUI
 
-let maxCount = 999
-
-enum ButtonVariants {
-    case res
-    case inc
-}
-
 func getRandomEmoji() -> String {
     return String(UnicodeScalar(Array(0x1F300...0x1F3F0).randomElement()!)!)
 }
 
 struct ContentView: View {
+    private let maxCount = 999
+
     @AppStorage("count") private var count: Int = 0
     @AppStorage("emoji") private var emoji: String = getRandomEmoji()
     @State private var isShowingConfirmReset: Bool = false
-    
+
     var body: some View {
         ZStack {
             Color(.black).edgesIgnoringSafeArea(.all)
-            
+
             VStack(alignment: .center, spacing: 120) {
                 Spacer()
                 createCounterText()
                 Spacer()
-                
+
                 HStack(alignment: .bottom, spacing: 24) {
-                    createButton(ButtonVariants.inc)
-                    createButton(ButtonVariants.res)
+                    createAddButton()
+                    createResetButton()
                 }
             }
+            .padding()
         }
     }
-    
-    
+
     private func createCounterText() -> Text {
         return Text("\(emoji) \(count) \(emoji)")
             .foregroundColor(.white)
             .font(Font.system(size: 68))
     }
-    
-    private func createButton(_ btnVariant: ButtonVariants) -> some View {
+
+    private func createAddButton() -> some View {
         return Button {
-            switch btnVariant {
-            case .res:
-                isShowingConfirmReset = true
-            case .inc:
-                if count < maxCount {
-                    count += 1
-                }
-                emoji = getRandomEmoji()
+            if count < maxCount {
+                count += 1
             }
+            emoji = getRandomEmoji()
         } label: {
             let isMaxCount = count == maxCount
             let incBtnLabel = isMaxCount ? "Emoji" : "Add"
             let incBtnImage = isMaxCount ? "heart" : "plus"
-            
-            Label(btnVariant == .inc ? incBtnLabel : "Reset", systemImage: btnVariant == .inc ? incBtnImage : "arrow.2.squarepath")
+
+            Label(incBtnLabel, systemImage: incBtnImage)
+                .font(Font.system(size: 24))
+                .frame(width: 140, height: 40)
+        }
+        .buttonStyle(.bordered)
+        .tint(.cyan)
+    }
+
+    private func createResetButton() -> some View {
+        return Button {
+            isShowingConfirmReset = true
+        } label: {
+            Label("Reset", systemImage: "arrow.2.squarepath")
                 .font(Font.system(size: 24))
                 .frame(width: 140, height: 40)
         }
@@ -70,9 +72,9 @@ struct ContentView: View {
                 emoji = getRandomEmoji()
             }
         }
+        .disabled(count == 0)
         .buttonStyle(.bordered)
-        .tint(btnVariant == .inc ? .cyan : .pink)
-        .disabled(btnVariant == .res && count == 0)
+        .tint(.pink)
     }
 }
 
